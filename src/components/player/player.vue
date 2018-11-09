1eaf92f84744176b1481af0ee2c2afb06077dbc4
xuyanyfb <821224>
2018-11-09 15:35:51 +0800

    打包发布

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -97,7 +97,7 @@
       </div>
     </transition>
     <audio :src="currentSong.url" ref="audio"
-           @canplay="ready" @error="error"
+           @play="ready" @error="error"
            @timeupdate="timeupdate" @ended="ended"></audio>
   </div>
 </template>
@@ -233,6 +233,7 @@
         }
         if (this.playList.length === 1) {
           this.loop()
+          return
         } else {
           let index = this.currentIndex - 1
           if (index === -1) {
@@ -251,6 +252,7 @@
         }
         if (this.playList.length === 1) {
           this.loop()
+          return
         } else {
           let index = this.currentIndex + 1
           if (index === this.playList.length) {
@@ -333,6 +335,9 @@
       },
       getLyric() {
         this.currentSong.getLyric().then((lyric) => {
+          if (this.currentSong.lyric !== lyric) {
+            return
+          }
           this.currentLyric = new Lyric(lyric, this.handleLyric)
           if (this.playing) {
             this.currentLyric.play()
@@ -415,13 +420,20 @@
     },
     watch: {
       currentSong(newSong, oldSong) {
+        if (!newSong.id) {
+          return
+        }
         if (newSong.id === oldSong.id) {
           return
         }
         if (this.currentLyric) {
           this.currentLyric.stop()
+          this.currentTime = 0
+          this.currentLineNum = 0
+          this.playingLyric = ''
         }
-        setTimeout(() => {
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
           this.$refs.audio.play()
           this.getLyric()
         }, 1000)
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from 'components/rank/rank'
-import Recommend from 'components/recommend/recommend'
-import Search from 'components/search/search'
-import Singer from 'components/singer/singer'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
 
 Vue.use(Router)
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((rank) => {
+    resolve(rank)
+  })
+}
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((rank) => {
+    resolve(rank)
+  })
+}
+const Search = (resolve) => {
+  import('components/search/search').then((search) => {
+    resolve(search)
+  })
+}
+const Singer = (resolve) => {
+  import('components/singer/singer').then((singer) => {
+    resolve(singer)
+  })
+}
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((singerdetail) => {
+    resolve(singerdetail)
+  })
+}
+const Disc = (resolve) => {
+  import('components/disc/disc').then((disc) => {
+    resolve(disc)
+  })
+}
 
 export default new Router({
   routes: [
